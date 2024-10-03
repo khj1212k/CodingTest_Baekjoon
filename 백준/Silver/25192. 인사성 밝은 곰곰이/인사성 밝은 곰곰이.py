10d6b018634c1fdbc03ec9cb8be8c7f6abd88f7b
khj1212k <58841556+khj1212k@users.noreply.github.com>
2024-10-03 17:27:36 +0800

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 76 ms, Memory: 42388 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -1,14 +1,16 @@
-# 인사성 밝은 곰곰이 
+import sys
+
 cnt = 0
 li = set()
 
-n = int(input())
+# 전체 입력을 한 번에 처리
+n = int(sys.stdin.readline())
 for _ in range(n):
-    name = input()
+    name = sys.stdin.readline().strip()  # 입력값의 개행 문자를 제거
     if name == "ENTER":
-        li = set()
-    elif not(name in li):
+        li.clear()  # 재할당 대신 set을 비우는 방법
+    elif name not in li:
         li.add(name)
-        cnt+=1
-    
+        cnt += 1
+
 print(cnt)
