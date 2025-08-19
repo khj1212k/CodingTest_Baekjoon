44566588d908834020decd0531a38dd1cd541b27
khj1212k <58841556+khj1212k@users.noreply.github.com>
2025-08-19 22:13:59 +0900

    [level 2] Title: 괄호 회전하기, Time: 110.84 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,28 +1,17 @@
-def is_correct_brackets(s):
+def is_correct(s):
+    opn, cls = '([{', ')]}'
     stack = []
-    opn = '([{'
-    cls = ')]}'
     
     for br in s:
-        if br in opn:
+        if br in opn: # 여는 괄호
             stack.append(br)
-        else:
-            if not stack:
+        else: # 닫는 괄호 
+            if not stack: # 닫는괄호가 들어왔는데 스택이 비어있는 경우
                 return False
-            
-            top = stack.pop()
-            if cls.index(br) != opn.index(top):
+            top = stack.pop() # 스택이 안 빈 경우
+            if cls.index(br) != opn.index(top): # 괄호의 종류가 다를경우
                 return False
+    return not stack # 순환이 끝나고 스택이 비었으면 정상True, 아직 남았으면 False
     
-    return not stack
-
 def solution(s):
-    n = len(s)
-    answer = 0
-    
-    for i in range(n):
-        rotated_s = s[i:] + s[:i]
-        if is_correct_brackets(rotated_s):
-            answer += 1
-            
-    return answer
+    return sum(1 for i in range(len(s)) if is_correct((s*2)[i:i+len(s)]))
