78bba1417bfda98bd67341b9f999f51f62ef38aa
khj1212k <58841556+khj1212k@users.noreply.github.com>
2025-08-19 21:28:14 +0900

    [level 2] Title: 할인 행사, Time: 337.13 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,12 +1,15 @@
 from collections import Counter
 
-def solution(want, number, discount):
-    n = sum(number)
-    want_c = Counter({k:v for k,v in zip(want,number)})
-    cnt = 0
+# def solution(want, number, discount):
+#     n = sum(number)
+#     want_c = Counter({k:v for k,v in zip(want,number)})
+#     cnt = 0
     
-    for i in range(len(discount)-n+1):
-        if want_c == Counter(discount[i:i+n]):
-            cnt+=1
+#     for i in range(len(discount)-n+1):
+#         if want_c == Counter(discount[i:i+n]):
+#             cnt+=1
 
-    return cnt
+#     return cnt
+
+def solution(want, number, discount):
+    return sum(1 for i in range(len(discount)-sum(number)+1) if Counter({k:v for k,v in zip(want,number)}) == Counter(discount[i:i+sum(number)]))
