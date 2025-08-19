2f9e88b19bbd4c26de083cf882e2f307c1eea7f7
khj1212k <58841556+khj1212k@users.noreply.github.com>
2025-08-19 15:55:51 +0900

    [level 2] Title: 피보나치 수, Time: 142.29 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,10 +1,5 @@
 def solution(n):
-    if n == 0: return 0
-    F = [0] * (n+1)
-    
-    F[0],F[1] = 0,1
-    
-    for i in range(2,n+1):
-        F[i] = (F[i-1] + F[i-2]) % 1234567
-
-    return F[n]
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a+b
+    return a % 1234567
