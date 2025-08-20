e771d835e0c1bde108f5b1f8989c03cc71fea51b
khj1212k <58841556+khj1212k@users.noreply.github.com>
2025-08-20 14:47:15 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,29 @@
+# 남은일수 : ((100-progress) / speed)의 올림
+#          ((100-30) / 30) = 2
+
+# [5, 10, 1, 1, 20, 1]
+# 5:1, 10:3, 20:1
+# for l in left 에서 하나씩 제일큰수를 찾는다. max = l
+# 다음 더 큰수가 올때까지 cnt+=1
+# 그리고 그 다음 수가 더 큰수면 if l > max: answer.append(cnt), cnt = 1, max = l
+
+from math import ceil
+
+def solution(progresses, speeds):
+    answer = []
+    stack = []
+    left = [ceil((100-p)/s) for p, s in zip(progresses,speeds)]
+    print(left)
+    
+    cnt = 1
+    maxx = 0
+    for l in left:
+        if l > maxx:
+            answer.append(cnt)
+            cnt = 1
+            maxx = l
+        else : 
+            cnt += 1
+    answer.append(cnt)
+            
+    return answer[1:]
