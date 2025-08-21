dcc78c83bc08990a6f7b5ad4b845d490a4664dc9
khj1212k <58841556+khj1212k@users.noreply.github.com>
2025-08-21 15:37:02 +0900

    [level 3] Title: 정수 삼각형, Time: 35.30 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=python3) 
 
 ### 성능 요약
 
-메모리: 17.9 MB, 시간: 54.65 ms
+메모리: 14 MB, 시간: 35.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 21일 15:23:52
+2025년 08월 21일 15:36:59
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,28 +1,22 @@
-# 7
-#   10    8
-# 18 11(9) 18
-# 
-
-#   0
-#  0 1
-# 0 1 2
-# 
+# def solution(triangle):
+#     answer = 0
+#     ans = [[0]*(i+1) for i in range(len(triangle))]
+#     ans[0][0] = triangle[0][0]
+    
+#         for i in range(len(triangle)-1): # 전체리스트 위에서 아래로 idx
+            
+#             for j in range(len(ans[i])): # 윗리스트 순환 val, 아랫값 2개에 추가해서 ans에 넣을거  
+#                     if triangle[i+1][j]+ans[i][j] > ans[i+1][j]:# 기존 ans(오른쪽)
+#                         ans[i+1][j] = triangle[i+1][j]+ans[i][j] # ans(오른쪽) = 밑(오른쪽) + 위 
+#                     ans[i+1][j+1] = triangle[i+1][j+1]+ans[i][j] # ans(왼쪽) = 및(왼쪽) + 위 
+#                     pass
+#         return max(ans[-1])
 
 def solution(triangle):
-    answer = 0
-    ans = [[0]*(i+1) for i in range(len(triangle))]
-    ans[0][0] = triangle[0][0]
+        for d in range(-1,-len(triangle),-1): # 마지막줄부터 올라오며 순환 idx 
+            for i in range(len(triangle[d-1])): #그 윗리스트 순환 idx
+                triangle[d-1][i] += max(triangle[d][i],triangle[d][i+1])
+                
+        return triangle[0][0]
+
     
-    try:
-        for i in range(len(triangle)-1): # 전체리스트 위에서 아래로 idx
-            
-            for j in range(len(ans[i])): # 윗리스트 순환 val, 아랫값 2개에 추가해서 ans에 넣을거  
-                    if triangle[i+1][j]+ans[i][j] > ans[i+1][j]:# 기존 ans(오른쪽)
-                        ans[i+1][j] = triangle[i+1][j]+ans[i][j] # ans(오른쪽) = 밑(오른쪽) + 위 
-                    ans[i+1][j+1] = triangle[i+1][j+1]+ans[i][j] # ans(왼쪽) = 및(왼쪽) + 위 
-                    pass
-        return max(ans[-1])
-    
-    except Exception as e:
-        print(ans)
-        print(f"에러가 발생했습니다: {e}")
