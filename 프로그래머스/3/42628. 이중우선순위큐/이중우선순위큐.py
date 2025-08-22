2bd07b90a2b43b996445ab0f647b80aae80736d2
khj1212k <58841556+khj1212k@users.noreply.github.com>
2025-08-22 19:18:35 +0900

    [level 3] Title: 이중우선순위큐, Time: 135.61 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -3,13 +3,9 @@ def solution(operations):
         pq = []
         for ops in operations:
             o,v = ops.split(' ')
-            if o == 'I':
-                pq.append(int(v))
-                pq.sort()
-            else: # o == 'D'
-                if pq:
-                    if v == '-1':
-                        pq = pq[1:]
-                    else : # v == 1
-                        pq.pop() # 최댓값
+            
+            if o == 'I': [pq.append(int(v)), pq.sort()]
+            elif pq and o == 'D' and v == '-1': pq = pq[1:]
+            elif pq and o == 'D' and v == '1' : pq.pop()
+            
         return [max(pq),min(pq)] if pq else [0,0]
