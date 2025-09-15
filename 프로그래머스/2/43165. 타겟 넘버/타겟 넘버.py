0aab9b3e0e3a7c041c3057eb2334cdf6f99031ea
khj1212k <58841556+khj1212k@users.noreply.github.com>
2025-09-15 15:37:51 +0900

    [level 2] Title: 타겟 넘버, Time: 382.30 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,15 @@
+def solution(numbers, target):
+    answer = 0
+    summ = 0
+    
+    def dfs(idx, summ):
+        nonlocal answer
+        if idx == len(numbers):
+            if summ == target: answer += 1
+            return
+
+        dfs(idx+1, summ + numbers[idx])
+        dfs(idx+1, summ - numbers[idx])
+        
+    dfs(0,summ)
+    return answer
