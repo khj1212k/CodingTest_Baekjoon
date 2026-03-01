9bce15a585a2c4a0e941bb7f7b60a86b3c79b059
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-01 17:16:15 +0900

    [level 2] Title: 올바른 괄호, Time: 9.83 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,12 @@
 def solution(s):
     stack = []
-    
-    for i in s:
-        if i == '(': # 여는 괄호
+    for i in [i for i in s]:
+        if i == '(':
             stack.append(i)
-        elif i == ')': # 닫는 괄호
-            if not stack: # 닫는괄호가 왔는데 여는괄호가 하나도 없다면 그건 이미 올바른 괄호가 아니다
+        else : 
+            if stack == []:
                 return False
-            stack.pop() # 안비어있다면 스택에서 하나 빼주기
-
-    return not stack # 스택이 비었는가?
+            else: stack.pop()
+    if stack == []:
+        return True
+    else : return False
