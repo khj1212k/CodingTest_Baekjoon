e901986633861b8b327a8c23b4b0b0e5c31b4ea6
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-01 17:58:29 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.23 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,14 +1,7 @@
-from collections import Counter
-
 def solution(s):
-    zeros_cnt = 0
-    s = [i for i in s]
-    cnt = 0
-    while s != ['1']:
-        ones = Counter(s)['1']
-        zeros_cnt += Counter(s)['0']
-        s = [i for i in format(ones,'b')]
-        cnt += 1
-        
-        print( f'{cnt= } ', f'{zeros_cnt= } ', f'{s= }')
-    return [cnt, zeros_cnt]
+    cnt, zeros = 0, 0
+    while s != '1':
+        cnt+=1
+        zeros += s.count('0')
+        s = bin(s.count('1'))[2:]
+    return [cnt, zeros]
