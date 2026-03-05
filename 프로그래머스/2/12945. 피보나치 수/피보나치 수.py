66783d84c1e17f856b5525149ed92d4fc294876e
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-05 22:50:28 +0900

    [level 2] Title: 피보나치 수, Time: 137.86 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,9 +1,6 @@
 def solution(n):
-    # a:F(0), b:F(1)
     a, b = 0, 1
-    aa = 0
     
-    for _ in range(1, n):
-        a, b = b ,a+b
+    for _ in range(1, n): a, b = b ,a+b
         
     return b % 1234567
