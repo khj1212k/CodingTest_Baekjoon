e56feec5ab4f3413f2d9f61394cd7c682a767a01
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-06 01:44:19 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 9.01 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,9 +1,5 @@
-import math
+from math import gcd
+from functools import reduce
 
 def solution(arr):
-    lcm = arr[0]
-    for i in arr:
-        # A*B = LCM(A*B) * GCB(A*B)
-        lcm = i * lcm // math.gcd(i,lcm)
-
-    return lcm
+    return reduce(lambda a,b : a*b//gcd(a,b) ,arr)
