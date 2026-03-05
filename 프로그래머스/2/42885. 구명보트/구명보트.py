8bf636e7f047c0acc2339f1dbf90d6df11431b24
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-05 23:38:55 +0900

    [level 2] Title: 구명보트, Time: 10.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,15 +1,12 @@
 from collections import deque
+
 def solution(people, limit):
     answer = 0
     people = deque(sorted(people))
-    print(people)
-    # help(deque)
+    
     while people:
-        if len(people) > 1 and people[0] + people[-1] <= limit:
-            people.popleft()
-            people.pop()
-            answer += 1
-        else:
-            people.pop()
-            answer += 1
+        heavy = people.pop()
+        if people and heavy+people[0] <= limit: people.popleft()
+        answer += 1
+        
     return answer
