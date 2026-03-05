68fcb9ab50373378992acc31b3d5ee4ac44f78bc
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-05 23:35:55 +0900

    [level 2] Title: 구명보트, Time: 18.44 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,15 @@
+from collections import deque
 def solution(people, limit):
-    left , right = 0, len(people)-1
-    people.sort()
-    cnt = 0
-    
-    while left <= right:
-        cnt += 1
-        if (people[right]+people[left]) <= limit: # 최소무게+최고무게 <= limit 
-            left += 1
-            right -= 1
-        else: right -= 1 # 두사람합 무게초과면 최고무게가 타고나가
-    
-    return cnt
+    answer = 0
+    people = deque(sorted(people))
+    print(people)
+    # help(deque)
+    while people:
+        if len(people) > 1 and people[0] + people[-1] <= limit:
+            people.popleft()
+            people.pop()
+            answer += 1
+        else:
+            people.pop()
+            answer += 1
+    return answer
