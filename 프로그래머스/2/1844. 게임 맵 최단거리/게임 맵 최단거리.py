a5e9cfbd3d2758866b6448ee32f3714a40e5581e
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-06 21:35:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.49 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,21 @@
 from collections import deque
 
 def solution(maps):
-    # 상하좌우
-    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-    rows, cols = len(maps), len(maps[0])
-    
-    def bfs(x,y):
-        q = deque()
-        q.append((x,y))
-        
+        h, w = len(maps), len(maps[0])
+        dist = [[-1]*w for _ in range(h)]
+        dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0] # 상하좌우
+        q = deque([(0,0)])
+        dist[0][0] = 1
+
         while q:
             x, y = q.popleft()
-            
             for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
-                if not (0 <= nx < rows and 0 <= ny < cols): continue # 맵 벗어남
-                if maps[nx][ny] == 0 : continue # 벽 무시
-                if maps[nx][ny] == 1: # 처음 지나가는 거리
-                    maps[nx][ny] = maps[x][y] + 1
-                    q.append((nx,ny))
-                    
-        res = maps[rows - 1][cols - 1]
-        return -1 if res == 1 else res
-    
-    return bfs(0,0)
+                nx, ny = x+dx[i], y+dy[i]
+                if not(0<=nx<w and 0<=ny<h) : continue
+                if maps[ny][nx] == 0 or dist[ny][nx] != -1: continue
+                q.append([nx,ny])
+                dist[ny][nx] = dist[y][x] + 1
+
+        return dist[h-1][w-1]
+
+        
