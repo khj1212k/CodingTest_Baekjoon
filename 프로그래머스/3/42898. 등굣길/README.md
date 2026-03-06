5546bf4a859819951e6dac7fb67cab2bc7d7875e
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-06 22:34:50 +0900

    [level 3] Title: 등굣길, Time: 4.88 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 9.2 MB, 시간: 4.50 ms
+메모리: 9.39 MB, 시간: 4.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 17일 15:54:07
+2026년 03월 06일 22:34:48
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,3 +1,12 @@
 def solution(m, n, puddles):
     answer = 0
-    return answer
+    maps = [[0]*m for i in range(n)]
+    maps[0][0] = 1
+    for h in range(n):
+        for w in range(m):
+            if [w+1,h+1] in puddles or [h,w] == [0,0]: continue
+            left = maps[h][w-1] if w!=0 else 0
+            up = maps[h-1][w] if h!=0 else 0
+            maps[h][w] = left + up
+        
+    return maps[n-1][m-1] % 1000000007
