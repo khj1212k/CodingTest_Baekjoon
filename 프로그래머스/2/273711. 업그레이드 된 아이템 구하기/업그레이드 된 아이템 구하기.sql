537b8f8c6963e18ec249f1b30c334679eac4259c
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-07 12:44:48 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -10,7 +10,7 @@ WHERE PARENT_ITEM_ID IN (
     SELECT PARENT_ITEM_ID
     FROM ITEM_TREE as t
     JOIN ITEM_INFO as i
-    ON t.PARENT_ITEM_ID = i.ITEM_ID
-    WHERE RARITY = 'RARE'
+    ON t.PARENT_ITEM_ID = i.ITEM_ID # 부모 아이템의 희귀도를 봐야함.
+    WHERE RARITY = 'RARE' # 부모 아이템의 희귀도
 )
 ORDER BY t.ITEM_ID DESC
