8615da77c975a96095a7075f8882b03763858f2d
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-07 13:33:52 +0900

    [level 2] Title: 배달, Time: 1.49 ms, Memory: 9.73 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,25 +1,27 @@
 import heapq
 
-
 def solution(N, road, K):
-    INF = float('inf')
-    
     graph = [[] for _ in range(N+1)]
-    for a,b,w in road:
-        graph[a].append((b,w))
-        graph[b].append((a,w))
+    for a,b,c in road:
+        graph[a].append((b,c))
+        graph[b].append((a,c))
     
-    dist = [INF] * (N+1)
+    INF = float('inf')
+    dist = [INF]*(N+1)
     dist[1] = 0
-    pq = [(0,1)] # (거리, 노드)
+
+    pq = [(0,1)] # cur_dist, node
     
     while pq:
         cur_dist, node = heapq.heappop(pq)
-        if cur_dist > dist[node]: continue # 이미 더 짧은 거리로 방문 기록 있으면 넘김
+        
+        if cur_dist > dist[node]: continue
+        
         for next_node, cost in graph[node]:
-            new_dist = cur_dist + cost
+            new_dist = cur_dist+cost
             if new_dist < dist[next_node]:
                 dist[next_node] = new_dist
-                heapq.heappush(pq, (new_dist,next_node))
-
-    return sum(d <= K for d in dist)
+                heapq.heappush(pq, (new_dist, next_node))
+    
+    
+    return sum(d<=K for d in dist)
