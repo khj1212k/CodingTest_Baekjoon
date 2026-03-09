ae4a78705cca6ec317ba9c70cd1c1909b26d9d51
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-03-09 14:12:03 +0900

    [level 3] Title: 숫자 게임, Time: 55.84 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -5,15 +5,13 @@ def solution(A, B):
     ai, bi = 0,0
     score = 0
     
-    for _ in range(n):
-        for __ in range(n):
-            if ai==n or bi==n: return score
-            if sort_A[ai] < sort_B[bi]: # 작은숫자들 부터 비교, B의 숫자가 더 크다면 
-                score+=1
-                ai+=1 # 다음꺼 비교
-                bi+=1 # 다음꺼 비교
-            else: # B가 더 작다면
-                bi+=1
+    while ai < n and bi < n:
+        if sort_A[ai] < sort_B[bi]: # 작은숫자들 부터 비교, B의 숫자가 더 크다면 
+            score+=1
+            ai+=1 # 다음꺼 비교
+            bi+=1 # 다음꺼 비교
+        else: # B가 더 작다면
+            bi+=1
+            
+    return score    
     
-    
-    return 
