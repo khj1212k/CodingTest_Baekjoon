453591d4f0c333b30cac16d0c365de930af05495
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-04-11 16:17:48 +0900

    [level 3] Title: 스티커 모으기(2), Time: 74.15 ms, Memory: 13.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,14 @@
+def dp(sticker):
+    dp = [0] * len(sticker)
+    
+    for i in range(len(sticker)):
+        if   i==0: dp[0] = sticker[0]
+        elif i==1: dp[1] = max(dp[0], sticker[1])
+        else     : dp[i] = max(dp[i-1], dp[i-2] + sticker[i])
+        
+    return dp[-1]
+
+def solution(sticker):
+    return max(dp(sticker[1:]), dp(sticker[:-1])) if len(sticker) != 1 else sticker[0]
+    
+    
