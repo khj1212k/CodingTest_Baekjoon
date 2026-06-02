df5c0ea31aa2d7f7b04848f3054b0facdeba7265
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-06-02 14:50:09 +0900

    [level 2] Title: 튜플, Time: 17.98 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -2,4 +2,4 @@ import re
 from collections import Counter
 
 def solution(s):
-    return list(map(int, [k for k,v in sorted(Counter(re.findall(r'\d+', s)).items(), key=lambda x : x[1], reverse=True)]))
+    return [int(k) for k,v in Counter(re.findall(r'\d+', s)).most_common()]
