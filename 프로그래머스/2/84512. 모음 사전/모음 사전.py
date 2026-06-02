dbc9335cf4c77b5e67f0804a3d085d49e5a2f936
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-06-02 15:24:22 +0900

    [level 2] Title: 모음 사전, Time: 2.09 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,5 +1,12 @@
-from itertools import product
-
 def solution(word):
-
-    return sorted(''.join(p) for n in range(1, 6) for p in product('AEIOU', repeat=n)).index(word) + 1
+    words = []
+    
+    def dfs(current):
+        if len(current) > 5:  return 
+        
+        words.append(current)
+            
+        for v in 'AEIOU':  dfs(current + v)
+    
+    dfs("")
+    return words.index(word)
