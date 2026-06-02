e80821c479ca9e9e6b7215e20a864e3aa6185ca0
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-06-02 14:13:52 +0900

    [level 2] Title: 롤케이크 자르기, Time: 265.25 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,20 +1,19 @@
-from collections import defaultdict
+from collections import Counter
 
 def solution(topping):
     answer = 0
-    left = set() # 기준선 왼쪽에 있는 토핑의 종류
-    right = defaultdict(int)
-    
-    for i in topping:
-        right[i] += 1
+    left = set()
+    right = Counter(topping)
     
     for t in topping:
         left.add(t)
         
-        if right[t] == 1:  del right[t] 
-        else:              right[t] -= 1
+        if right[t] == 1:
+            right.pop(t)
+        else:
+            right[t] -= 1
         
-        if len(left) == len(right): 
+        if len(right) == len(left):
             answer += 1
-        
+            
     return answer
