13921cfddc9f68d280581b1cb32ffc47cba24393
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-07-17 14:25:57 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 34.16 ms, Memory: 18.3 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,29 +1,30 @@
 def solution(gems):
     total_types = len(set(gems))
-    counts = {}
+    included_types = 0
     
     left = 0
-    best_start = 0
-    best_end = len(gems) - 1
-    included_types = 0
-
+    best_left = 0
+    best_right = len(gems) - 1
+    
+    counts = {}
+    
     for right, gem in enumerate(gems):
+        # right move
         if counts.get(gem, 0) == 0:
             included_types += 1
+            
+        counts[gem] = counts.get(gem,0) + 1
         
-        counts[gem] = counts.get(gem, 0) + 1
-
-        while included_types == total_types:
-            if right - left < best_end - best_start:
-                best_start = left
-                best_end = right
-
+        # left move
+        while included_types == total_types: # all type in dict
+            if right - left < best_right - best_left:
+                best_right, best_left = right, left
+            
             left_gem = gems[left]
             counts[left_gem] -= 1
-
-            if counts[left_gem] == 0:
-                included_types -= 1
-
+            
+            if counts[left_gem] == 0: included_types -= 1
+            
             left += 1
-
-    return [best_start + 1, best_end + 1]
+    
+    return [best_left+1, best_right+1]
