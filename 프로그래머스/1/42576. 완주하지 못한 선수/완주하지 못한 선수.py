830bd8ab9307b29bbb900342da7b4397c0fbfe13
khj1212k <58841556+khj1212k@users.noreply.github.com>
2026-07-20 20:45:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.08 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,31 +1,8 @@
-# def solution(participant, completion):
-#     res = ''
-
-#     participant.sort()
-#     completion.sort()
-
-#     for i in range(len(completion)):
-#         if participant[i] != completion[i]:
-#             res = participant[i]
-#             break
-
-#     if not res:
-#         res = participant[-1]
-
-#     return res
 def solution(participant, completion):
-
-    temp = 0
-    p = dict()
-    c = dict()
-
-    for i in participant:
-        p[hash(i)] = i
-        temp += hash(i)
-
-    for i in completion:
-        # c[hash(i)] = i
-        temp -= hash(i)
-
+    answer = ''
+    participant.sort()
+    completion.sort()
+    for p,c in zip(participant, completion+[""]):
+        if p != c:
+            return p        
     
-    return p[temp]
